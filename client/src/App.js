f53ade3e0483b9c41d129350f2e489b65dfd1979
Abhishek Singh <abhisheksatyam123@gmail.com>
2024-09-24 21:29:06 +0530

    Added React-router dom Added outlet funcinality and routing funcinality Created basic pages for : Login, Home , Write and etc..

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,66 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Single from "./pages/Single"
+import Write from "./pages/Write"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
+
+const Layout = () =>{  // a outlet functio which a parent can paas to its childrens. 
+  return (
+  <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+  </>
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children : [
+        {
+            path : "/Home",
+            element : <Home/>
+        },
+        {
+        path : "/post/:id",
+            element : <Single/>
+        },
+         {
+            path : "/Write",
+            element : <Write/>
+        },
+    ]
+  },
+  {
+    path: "/Register",
+    element: <Register/>,
+  },
+  {
+    path: "/Login",
+    element: <Login/>,
+  },
+
+]);
+
+
+
 export function App() {
   return (
     <div>
       Hello world!
+      <RouterProvider router={router}/>
     </div>
   );
 }
@@ -17,4 +76,3 @@ export function Temp2() {
 
 
 export default App;
-Temp2;
---
+++ notes.md
@@ -0,0 +1,3 @@
+# Creating react router 
+
+
