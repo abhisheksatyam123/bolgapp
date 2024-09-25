a4eac0aa41d253b0625226b222c379641a51b273
Abhishek Singh <abhisheksatyam123@gmail.com>
2024-09-25 22:53:38 +0530

    Added Loging and Register Page with css class

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,17 +4,18 @@ import { Link, } from "react-router-dom";
 const Login = () => {
 	return (
 		<div className='auth'>
-		<h1> Login </h1>
-		<form>
-			<input type="text" placeholder='username'/>
-			<input type="password" placeholder='password'/>
-			<button>Login </button>
-		</form>
-		<p>This is an error</p>
-		<span>
-		You dont have and account
-		<Link to="./register">Register</Link>
-		</span>
+			<h1> Login </h1>
+			<form>
+				<input Required type="text" placeholder='username'/>
+				<input Required type="password" placeholder='password'/>
+				<button>Login </button>
+				<p>This is an error</p>
+				<span>
+					You dont have and account
+					<Link to="./register">Register</Link>
+				</span>
+
+			</form>
 		</div>
 	)
 }
